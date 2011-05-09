56e88c2c59246c0524265c52f0129a43e6cc6fc3
Jeremy Henty <jh13@sanger.ac.uk>
2011-05-09 13:41:04 +0100

    anacode-perldoc.el: add documentation.

--- elisp/anacode-perldoc.el
+++ elisp/anacode-perldoc.el
@@ -1,7 +1,22 @@
+;;; anacode-perldoc.el -- run perldoc on Perl source
 
-(provide 'anacode-perldoc)
+;; Author: Ana Code <anacode@sanger.ac.uk>
+;; Keywords: perl perldoc
 
-(defun anacode-perldoc ()
+(defconst anacode-perldoc nil
+  "The documentation for the anacode\\=-perldoc library.
+Commands:
+  \\<perl-mode-map>\\[anacode-perldoc-run] - run perldoc on your Perl code.
+    mnemonic: (d)oc
+    details: `anacode-perldoc-run'")
+
+(defun anacode-perldoc-run ()
+  "Run perldoc on your Perl source.
+This runs perldoc on the contents of the buffer and
+displays the results in an alternative buffer.
+Unsaved changes are saved first.
+Perldoc is run with the text formatter so the results
+are not identical to running perldoc on the terminal."
   (interactive)
   (if (eq major-mode 'perl-mode)
       (let ((file (buffer-file-name)))
@@ -12,9 +27,16 @@
               (with-output-to-temp-buffer buffer-name
                 (with-current-buffer (get-buffer buffer-name)
                   (call-process "perldoc" nil t t "-t" file)))))))
-    (message "The buffer %s does not appear to contain Perl code!" (buffer-name))))
+    (message
+     "The buffer %s does not appear to contain Perl code!"
+     (buffer-name))))
 
 (defun anacode-perldoc-perl-mode-hook ()
-  (local-set-key [?\C-c ?d] 'anacode-perldoc))
+  "The anacode-perldoc.el hook function for Perl mode."
+  (local-set-key [?\C-c ?d] 'anacode-perldoc-run))
 
 (add-hook 'perl-mode-hook 'anacode-perldoc-perl-mode-hook)
+
+(provide 'anacode-perldoc)
+
+;;; anacode-perldoc.el ends here
