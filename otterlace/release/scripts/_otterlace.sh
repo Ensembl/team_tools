758aad78ca377c0431c33dc5809829bc7db9c235
Matthew Astley <mca@sanger.ac.uk>
2011-10-18 15:33:19 +0100

    extend _otterlace.sh to know where files go.
    new full_version() give major.minor various ways;
    new otter_ipath_get is hard-wired to /software/anacode,
      like the existing build scripts;
    define wrapperfile_outside_otterdir() as a function,
      huge comment explaining the relevant factors can hopefully go Away
      later when we have proven install tools
    
    new scripts/otterlace_showvars to exercise it;
    prove(1)-able code to nail down the corners;

--- otterlace/release/scripts/_otterlace.sh
+++ otterlace/release/scripts/_otterlace.sh
@@ -80,6 +80,24 @@ config_show_maybe() {
 }
 
 
+# Default output is "$major.$minor"
+#
+# Separator is always omitted if minor version is empty.
+#
+# Assumes the versions have the correct number of digits, and does no
+# arithmetic on them.  (Beware the octal-like prefix.)
+#
+# Beware also that the optional minor & major args are reversed, to
+# allow setting just the minor
+full_version() {
+    local sep pfx to_var minor major
+    sep="${1:-.}"
+    pfx="$2"
+    to_var="${3:+-v $3}"
+    minor="${4:-$( config version_minor )}"
+    major="${5:-$( config version_major )}"
+    printf $to_var "%s%s%s%s" "$pfx" "$major" "${minor:+$sep}" "$minor"
+}
 
 git_show_maybe() {
     if [ -n "$verbose" ]; then
@@ -101,6 +119,118 @@ _whatami() {
     ciid="$(     cd "$dist_scripts" && git log -1 --format=%h             || echo '??' )"
     echo "$0 ($ciid from $upstream)"
 }
+
+# Control of where the build is put - use it instead of hardwired
+# install destinations.
+#
+# Writes to variable named by $1 or emits to STDOUT if $1=""
+# Path type is chosen by $2 which defaults to $1
+#
+# Design aims: run in-line with the caller, not in a subshell; avoid
+# recursion; avoid duplicated calculations; avoid array variables.
+otter_ipath_get() {
+    local __varname __key __out \
+         swac full_version \
+        _oig_holtdir _oig_otter_home _oig_bin _oig_wrapperfile
+
+    __varname="$1"
+    __key="${2:-$1}"
+    [ -z "$__varname" ] && __varname='__out' # will send to STDOUT
+
+    swac=/software/anacode
+    full_version="$( full_version )" || bail "Cannot get version number"
+
+
+    # Prepare all relevant paths once, up front...  else we're tempted
+    # to recurse or duplicate the logic
+    printf -v _oig_holtdir         %s/otter       "$swac"
+    printf -v _oig_otter_home      %s/otter_rel%s "$_oig_holtdir" "$full_version"
+    printf -v _oig_bin             %s/bin         "$_oig_otter_home"
+
+    if wrapperfile_outside_otterdir; then
+        printf -v _oig_wrapperfile %s/bin/otterlace_rel%s "$swac" "$full_version"
+    else
+        # v58+ : wrapper in bin
+        printf -v _oig_wrapperfile %s/otterlace   "$_oig_bin"
+    fi
+
+
+    # Do the outputting.  Also document what these are.
+    case "$__key" in
+        holtdir)
+            # The directory in which the "otter_home"s are collected.
+            # For internal Linux releases, it has been hardwired here.
+            # It will become more flexible soon.
+            printf -v "$__varname" %s "$_oig_holtdir" ;;
+        otter_home)
+            # Directory where self-contained Otterlace is installed.
+            printf -v "$__varname" %s "$_oig_otter_home" ;;
+        bin)
+            # Directory for all binaries and scripts EXCEPT outermost
+            # 'otterlace' wrapper script.  This is inside otter_home.
+            printf -v "$__varname" %s "$_oig_bin" ;;
+        wrapperfile)
+            # Complete filename for the 'otterlace' wrapper script.
+            # This needs to become version, arch & OS dependent.
+            printf -v "$__varname" %s "$_oig_wrapperfile" ;;
+        *) bail "otter_ipath_get: unknown key '$__key'"
+    esac
+    [ "$__varname" = '__out' ] && printf %s "$__out"
+}
+
+
+# Control of the location of the outermost wrapper script...
+#
+# In "old" releases, $swac/bin/otterlace_rel$VSN
+# in "new" releases, $OTTERHOME/bin/otterlace (with a symlink pointing in)
+#
+# Changing at the same time is the source for the wrapperfile.
+#    In ensembl-otter commit 0fe1ed84
+#    (early after humpub-release-58-dev)
+#       dist/templates/otterlace moved to
+#       scripts/client/otterlace
+#
+# Related is the split of otterlace_env.sh from otterlace (0833c7eb)
+#    "dependence on $0 being the file itself not a symlink to the
+#    file" arises from the sourcing of otterlace_env.sh, which is
+#    commit-atomic with the split...  i.e. no need to consider that
+#    here.
+#
+#    "otterlace_env.sh is available in the same directory as
+#    otterlace" corresponds exactly with installation at
+#    $OTTER_HOME/bin/otterlace .  Installing bin/otterlace outside
+#    $OTTER_HOME would break it; doing it for v58+ is conservatively
+#    safe.
+#
+# In short, where it should be installed was already decoupled from
+# its origin before v57; in v58+ (specifically after 0833c7eb) it MUST
+# be installed in $OTTER_HOME/bin .
+#
+#
+# otterlace/release/scripts/otterlace_build installs it, and uses
+# presence of dist/templates/otterlace in the source to decide where.
+#
+# otterlace/release/scripts/otterlace_symlink_update uses the presence
+# of pre-existing wrapper at $swac/bin/otterlace_rel${version} , which
+# follows on from the decision during otterlace_build .
+#
+# This function prefers not to rely on presence of ensembl-otter
+# source for the decision, for automated test stability.
+wrapperfile_outside_otterdir() {
+    [ "$( config version_major )" -lt 58 ]
+}
+# There are two ways it is to be used.  Consider how it differs from
+# master,
+#
+#    - where should otterlace_build put wrapperfile?
+#      In this, we prefer to put it "inside" even for the earliest v58
+#      (only relevant for rebuilding old)
+#
+#    - what/where should otterlace_symlink_update link?
+#      It's possible an early v58 could be rebuilt using current
+#      master team_tools, putting wrapperfile "outside".  This is
+#      unlikely enough to ignore.
+
 
 
 # Assumes we were called by script in this directory.  $0 is not this file!
---
+++ otterlace/release/t/showvars.t
@@ -0,0 +1,130 @@
+#! /usr/bin/env perl
+
+use strict;
+use warnings;
+use Test::More tests => 8;
+use File::Temp qw( tempdir );
+use File::Slurp qw( write_file );
+use Cwd 'abs_path';
+
+=head1 NAME
+
+showvars.t - sanity checks on output of _otterlace.sh
+
+=head1 DESCRIPTION
+
+These tests make tempdirs containing enough dist/conf/* to pretend
+they are ensembl-otter, so we can ensure that builds of all ages and
+platforms are represented.
+
+It should not matter what $PWD is when this script runs.
+
+=cut
+
+
+# absolute form of the otterlace/release/ parent directory; contains scripts/ and t/
+my $ottrel;
+
+my $verbose = $ENV{HARNESS_IS_VERBOSE} || !$ENV{HARNESS_ACTIVE};
+
+sub set_ottrel {
+    # This must happen before with_temp does chdir
+    $ottrel = abs_path($0);
+    $ottrel =~ s{/t/showvars\.t$}{}
+      or die "Cannot construct otterlace/release/ path from $ottrel";
+    diag "  ottrel is $ottrel" if $verbose;
+}
+
+sub with_temp {
+    my ($put_configs, $code, @code_args) = @_;
+
+    my $tempdir = tempdir('showvars.t.XXXXXX', CLEANUP => 1, TMPDIR => 1);
+    diag "  tempdir is $tempdir" if $verbose;
+    chdir $tempdir or die "chdir to tmpdir: $!";
+    (mkdir 'dist') && (mkdir 'dist/conf') or die "mkdir -p dist/conf: $!";
+
+    while (my ($k, $v) = each %$put_configs) {
+        write_file("dist/conf/$k", { atomic => 1 }, "$v\n");
+    }
+
+    $code->(@code_args);
+
+    # Out, so we can delete it after.  This incidentally means that
+    # failing tests leave the evidence behind.
+    chdir "$tempdir/..";
+}
+
+sub showvars_tt {
+    my ($name, $constpart) = @_;
+
+    my $got = qx{ $ottrel/scripts/otterlace_showvars };
+
+    # Take out variable parts, before comparing against template
+    $got =~ s{^thisprog=(.*)$}{(TP)}m;
+    my $thisprog = $1;
+
+    is $got, $constpart, "$name: compare constant part";
+    like($thisprog,
+         qr{^/.*/otterlace_showvars \([0-9a-f]+ from \S+:\S+/team_tools\.git\)$},
+         "$name: thisprog");
+}
+
+
+sub main {
+    set_ottrel();
+
+    my $swac = "\x2Fsoftware\x2Fanacode"; # hide from grep
+
+    ### Release versions, pre- and post- 58
+    #
+    with_temp({ version_major => 56, version_minor => '02' },
+              \&showvars_tt, "$swac 56.02", <<"TXT");
+(TP)
+full_version=56.02
+otter install paths:
+ holtdir=$swac/otter
+ otter_home=$swac/otter/otter_rel56.02
+ bin=$swac/otter/otter_rel56.02/bin
+ wrapperfile=$swac/bin/otterlace_rel56.02
+TXT
+
+    with_temp({ version_major => 58, version_minor => '07' },
+              \&showvars_tt, "$swac 58.07", <<"TXT");
+(TP)
+full_version=58.07
+otter install paths:
+ holtdir=$swac/otter
+ otter_home=$swac/otter/otter_rel58.07
+ bin=$swac/otter/otter_rel58.07/bin
+ wrapperfile=$swac/otter/otter_rel58.07/bin/otterlace
+TXT
+
+
+    ### Dev versions
+    #
+    with_temp({ version_major => 56, version_minor => '' },
+              \&showvars_tt, "$swac 56 dev", <<"TXT");
+(TP)
+full_version=56
+otter install paths:
+ holtdir=$swac/otter
+ otter_home=$swac/otter/otter_rel56
+ bin=$swac/otter/otter_rel56/bin
+ wrapperfile=$swac/bin/otterlace_rel56
+TXT
+
+    with_temp({ version_major => 58, version_minor => '' },
+              \&showvars_tt, "$swac 58 dev", <<"TXT");
+(TP)
+full_version=58
+otter install paths:
+ holtdir=$swac/otter
+ otter_home=$swac/otter/otter_rel58
+ bin=$swac/otter/otter_rel58/bin
+ wrapperfile=$swac/otter/otter_rel58/bin/otterlace
+TXT
+
+
+}
+
+main();
