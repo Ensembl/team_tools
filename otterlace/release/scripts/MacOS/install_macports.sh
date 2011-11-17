9ae744cec8fdde8d507c6c17b95d0c0d08c50423
Michael Gray <mg13@mib103146i.internal.sanger.ac.uk>
2011-11-17 14:25:37 +0000

    Patch MacPorts config.

--- otterlace/release/scripts/MacOS/install_macports.sh
+++ otterlace/release/scripts/MacOS/install_macports.sh
@@ -3,6 +3,10 @@
 set -e # bail out on error
 
 # . "$( dirname "$0" )/../_otterlace.sh" || exit 1
+script_base="$( dirname "$0" )"
+
+# assume we're being run in the top-level app dir, e.g. otterlace.app
+app_base="${PWD}"
 
 macports_url_base="https://distfiles.macports.org/MacPorts"
 macports_ver="2.0.3"
@@ -11,8 +15,14 @@ macports_name="MacPorts-${macports_ver}"
 macports_tarball="${macports_name}.tar.bz2"
 macports_download="${macports_url_base}/${macports_tarball}"
 
-install_base="${PWD}"
-work_dir="${install_base}/../_macports_src"
+# based on this script being in $TT/otterlace/release/scripts/MacOS ...
+etc_base="${script_base}/../../etc"
+macports_patch="${etc_base}/MacOS/macports.patch"
+
+resource_path="Contents/Resources"
+install_base="${app_base}/${resource_path}"
+
+work_dir="${app_base}/../_macports_src"
 
 # ensure we don't pick up any previous MacPorts installation
 export PATH=/bin:/sbin:/usr/bin:/usr/sbin
@@ -42,10 +52,15 @@ fi
 make
 make install
 
-echo "Need to modify config here"
+echo "Patching MacPorts config"
+
+cd "${install_base}"
+patch -p0 < "${macports_patch}"
+
 echo
 echo "Now connect to non-rsync-firewalled network and run:"
 echo "  ${install_base}/bin/port selfupdate"
 
 exit $?
 
+# EOF
