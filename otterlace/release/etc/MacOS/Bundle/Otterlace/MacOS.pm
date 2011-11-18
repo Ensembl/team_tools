199397db0c9a5b4ccecba4f736cfd6a0845305d7
Michael Gray <mg13@mib103146i.internal.sanger.ac.uk>
2011-11-18 17:17:05 +0000

    First cut of install_cpan_bundle.sh & support.

---
+++ otterlace/release/etc/MacOS/Bundle/Otterlace/MacOS.pm
@@ -0,0 +1,12 @@
+package Bundle::Otterlace::MacOS 1.00;
+
+=head1 CONTENTS
+
+Ace
+File::Spec::Link
+Bio::Seq
+Proc::ProcessTable
+
+=cut
+
+1;
---
+++ otterlace/release/scripts/MacOS/install_cpan_bundle.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -e # bail out on error
+
+. "$( dirname "$0" )/_macos.sh" || exit 1
+
+eval $( "$install_base/bin/perl" -V:version ) # sets $version
+perl_ver=$version
+unset version
+echo "Perl version is ${perl_ver}"
+perl5_lib="${install_base}/lib/perl5"
+
+config_src="${etc_macos}/CPAN/Config.pm"
+config_dst="${perl5_lib}/${perl_ver}/CPAN/Config.pm"
+config_bak="${config_dst}.arm_bak"
+
+cpan_home="${install_base}/var/cpan"
+
+[ -e "${config_dst}" ] && mv "${config_dst}" "${config_bak}"
+
+sed -e "s|ARM_CPAN_HOME|${cpan_home}|" \
+    -e "s|ARM_INSTALL_BASE|${install_base}|" \
+   "${config_src}" \
+ > "${config_dst}"
+
+mkdir -v -p "${cpan_home}"
+
+bundle_src="${etc_macos}/Bundle/Otterlace/MacOS.pm"
+bundle_dir="${perl5_lib}/site_perl/${perl_ver}/Bundle/Otterlace"
+
+mkdir -v -p "${bundle_dir}"
+cp "${bundle_src}" "${bundle_dir}"
+
+exit 0
