9398737c4e501aae9e8952afd8452709c8d8f693
Jeremy Henty <jh13@sanger.ac.uk>
2012-02-10 15:46:42 +0000

    elisp/anacode-perl-fix.el: add.

--- elisp/anacode.el
+++ elisp/anacode.el
@@ -9,12 +9,14 @@ Features:
   `anacode-perl-syntax' - syntax check your Perl code.
   `anacode-perlcritic'  - run perlcritic on your Perl code.
   `anacode-perldoc'     - run perldoc on your Perl code.
-  `anacode-perltidy'    - run perltidy on the region.")
+  `anacode-perltidy'    - run perltidy on the region.
+  `anacode-perl-fix'    - fix your Perl code.")
 
 (require 'anacode-perl-syntax)
 (require 'anacode-perlcritic)
 (require 'anacode-perldoc)
 (require 'anacode-perltidy)
+(require 'anacode-perl-fix)
 
 (provide 'anacode)
 
