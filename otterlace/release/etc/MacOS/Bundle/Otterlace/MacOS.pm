e9e2acf8c547db5d23e2ed4c1c516563ac30776c
Michael Gray <mg13@sanger.ac.uk>
2012-03-08 16:05:24 +0000

    CPAN bundle: add Bio-BigFile.

--- otterlace/release/etc/MacOS/Bundle/Otterlace/MacOS.pm
+++ otterlace/release/etc/MacOS/Bundle/Otterlace/MacOS.pm
@@ -5,6 +5,7 @@ package Bundle::Otterlace::MacOS 1.00;
 Ace
 File::Spec::Link
 Bio::Root::Version - actually bioperl, need this to get actual version :-(
+Bio::DB::BigWig    - actual Bio-BigFile
 Proc::ProcessTable
 
 =cut
