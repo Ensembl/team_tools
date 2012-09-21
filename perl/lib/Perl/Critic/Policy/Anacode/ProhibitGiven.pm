9590200a64c1c2ff7712fe4711ac64ab999e4790
Jeremy Henty <jh13@sanger.ac.uk>
2012-09-21 10:55:29 +0100

    Anacode/ProhibitGiven.pm: a perlcritic polices that reports all uses of "given".

---
+++ perl/lib/Perl/Critic/Policy/Anacode/ProhibitGiven.pm
@@ -0,0 +1,24 @@
+
+package Perl::Critic::Policy::Anacode::ProhibitGiven;
+
+use strict;
+use warnings;
+
+use base qw( Perl::Critic::Policy );
+
+use Perl::Critic::Utils qw( :severities );
+
+sub supported_parameters { return (); }
+sub default_severity { return $SEVERITY_MEDIUM; }
+sub default_themes { return qw( bugs anacode ); }
+sub applies_to { return qw( PPI::Statement::Given ) };
+
+sub violates {
+    my ($self, $element, undef) = @_;
+    return $self->violation(
+        q("given" statement),
+        q("given" statements create strange behaviour and bugs.),
+        $element);
+}
+
+1;
