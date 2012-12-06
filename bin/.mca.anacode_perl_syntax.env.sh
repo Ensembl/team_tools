8b2ae3b808b79bb92969b1a58071a7c8eeeb3d4c
Matthew Astley <mca@sanger.ac.uk>
2012-12-06 17:19:24 +0000

    check in my anacode_perl_syntax config
    not used without the symlink,
      ln -s .$USER.anacode_perl_syntax.env.sh bin/anacode_perl_syntax.env.sh

---
+++ bin/.mca.anacode_perl_syntax.env.sh
@@ -0,0 +1,14 @@
+
+export PERL5LIB=\
+$ANACODE_TEAM_TOOLS/perl/lib\
+:~/gitwk-anacode/ensembl-otter/modules\
+:~/gitwk-anacode/ensembl-otter/modules/t/lib\
+:~/cvswork-anacode/PerlModules\
+:/software/anacode/pipeline/ensembl.for_pipe/modules\
+:/software/anacode/pipeline/ensembl-analysis/modules\
+:/software/anacode/pipeline/ensembl-pipeline/modules\
+:/software/anacode/pipeline/ensembl-pipeline/scripts/Finished/assembly\
+:/software/anacode/pipeline/bioperl-1.4\
+:/software/anacode/lib/site_perl\
+:/software/anacode/otter/otter_dev/lib/perl5\
+:/software/anacode/distro/$( lsb_release -sc )/lib/site_perl
