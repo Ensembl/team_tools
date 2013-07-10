b7be601868f10def88392d384a96def69585b569
Michael Gray <mg13@sanger.ac.uk>
2013-07-10 14:13:30 +0100

    otter-web-utils: import jh13's work.

---
+++ otter-web-utils/htdocs/assembly_adjacent.js
@@ -0,0 +1,46 @@
+
+$(document).ready(function() {
+
+    var
+      DBRootURL = "/cgi-bin/jh13/",
+      DBListURL = DBRootURL + "db_list",
+      DBAdjacenciesURL = DBRootURL + "assembly_adjacent";
+
+    var addDatabaseTR = function(tbody) {
+      var tr = $(document.createElement("tr"));
+      tbody.append(tr);
+      return tr;
+    };
+
+    var addDatabaseNameTD = function(db,tr) {
+      var td = $(document.createElement("td"));
+      tr.append(td);
+      td.text(db);
+      return td;
+    };
+
+    var addDatabaseAdjacenciesTD = function(query,tr) {
+      var td = $(document.createElement("td"));
+      tr.append(td);
+      td.addClass("progress_loading");
+      td.text("LOADING");
+      $.getJSON(DBAdjacenciesURL,query,function(adjacencies) {
+          td.removeClass("progress_loading"); 
+          td.addClass("progress_ok");
+          td.empty;
+          td.text(adjacencies.count);
+        });
+      return td;
+    };
+
+    var tbody = $(".db_list");
+    $.getJSON(DBListURL,function(dbs) {
+        $.each(dbs,function(i,db) {
+            var tr = addDatabaseTR(tbody);
+            addDatabaseNameTD(db,tr);
+            var query = { db: db, };
+            addDatabaseAdjacenciesTD(query,tr);
+          });
+      });
+
+  });
