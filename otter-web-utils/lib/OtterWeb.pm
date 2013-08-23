a157772ed4b5de31e6fb10b31128b6435fb9a496
Matthew Astley <mca@sanger.ac.uk>
2013-08-23 15:26:02 +0100

    remove db passwords - replace with Bio::Otter dependency
    assumes we are running in a webserver that
      also contains at least one Otter Server
    
    relies on webvm.git 3c1221bb for the "otter-dev" tag
    
    fixes RT#230065

--- otter-web-utils/lib/OtterWeb.pm
+++ otter-web-utils/lib/OtterWeb.pm
@@ -7,6 +7,10 @@ use warnings;
 use DBI;
 use base 'Exporter';
 
+use CGI::Carp 'fatalsToBrowser';
+use SangerPaths qw( otter-dev );
+use Bio::Otter::Server::Config;
+
 our @EXPORT_OK = qw( otterlive_dbh );
 
 
@@ -14,9 +18,12 @@ sub otterlive_dbh {
     my ($db) = @_;
     $db = $db ? ";database=$db" : "";
 
+    my $dataset = Bio::Otter::Server::Config->SpeciesDat->dataset('human');
+    my ($user, $pass) = @{ $dataset->params }{qw{ USER PASS }};
+
     return DBI->connect
       ("DBI:mysql:host=otterlive;port=3324;database=${db}",
-       "ottadmin", "xxx", { RaiseError => 1, AutoCommit => 0 } );
+       $user, $pass, { RaiseError => 1, AutoCommit => 0 } );
 }
 
 1;
