6485cdf16d2249dc3d8b7a0406f27f691c2e1a86
Michael Gray <mg13@sanger.ac.uk>
2013-11-19 17:05:47 +0000

    MacOS build: extra and updated ports and modules.

--- otterlace/release/etc/MacOS/Bundle/Otterlace/MacOS.pm
+++ otterlace/release/etc/MacOS/Bundle/Otterlace/MacOS.pm
@@ -6,8 +6,10 @@ Ace
 File::Spec::Link
 Bio::Root::Version - actually bioperl, need this to get actual version :-(
 Bio::DB::BigWig    - actual Bio-BigFile
+Bio::DB::Sam
 Proc::ProcessTable
 MooseX::Log::Log4perl
+Sys::LoadAvg
 
 =cut
 
