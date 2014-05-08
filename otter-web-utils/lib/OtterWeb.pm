4df534e7ca962c06fd093f98987b444ea8bc4724
Matthew Astley <mca@sanger.ac.uk>
2014-05-08 15:04:35 +0100

    otter-web-utils/locks - access to all datasets
    switch from q&d to species.dat & dataset objects
    
    cgi-bin/ds_list
      new script to enumerate datasets
      names are different to db_list

--- otter-web-utils/lib/OtterWeb.pm
+++ otter-web-utils/lib/OtterWeb.pm
@@ -9,9 +9,10 @@ use base 'Exporter';
 
 use CGI::Carp 'fatalsToBrowser';
 use SangerPaths qw( otter-dev );
+use Bio::Otter::Git qw{ :server_ensembl :match };
 use Bio::Otter::Server::Config;
 
-our @EXPORT_OK = qw( otterlive_dbh );
+our @EXPORT_OK = qw( otterlive_dbh speciesdat_dbh );
 
 
 sub otterlive_dbh {
@@ -24,6 +25,12 @@ sub otterlive_dbh {
     return DBI->connect
       ("DBI:mysql:host=otterlive;port=3324;database=${db}",
        $user, $pass, { RaiseError => 1, AutoCommit => 0 } );
+}
+
+sub speciesdat_dbh {
+    my ($dataset_name) = @_;
+    my $dataset = Bio::Otter::Server::Config->SpeciesDat->dataset($dataset_name);
+    return $dataset->otter_dba->dbc->db_handle;
 }
 
 1;
