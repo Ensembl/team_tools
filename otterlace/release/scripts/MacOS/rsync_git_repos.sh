c56c91e87a3367381400f71ab5ce03c2ef930179
Michael Gray <mg13@sanger.ac.uk>
2014-06-10 14:17:52 +0100

    rsync_git_repos.sh for Mac-in-a-rack builds.

---
+++ otterlace/release/scripts/MacOS/rsync_git_repos.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -e # bail out on error
+
+. "$( dirname "$0" )/_macos_common.sh" || exit 1
+
+hostname="$( hostname )"
+[ "$hostname" = "intcvs1" ] || bail "Must be run on intcvs1"
+
+cd "/repos/git/anacode"
+
+ssh_spec="otter@anacodeosx"
+remote_path="~/Otter/Git/Mirrors/anacode"
+
+# Comment these out to go live and chatty, or un-comment for testing
+# dry_run="--dry-run"
+verbose="--itemize-changes"
+
+do_rsync () {
+    local repo dest
+    repo="$1"
+    dest="$2"
+    dest="${remote_path}${dest:+/$dest}"
+    rsync -a --del "$dry_run" "$verbose" "$repo" "${ssh_spec}:${dest}"
+}
+
+do_rsync PerlModules.git
+do_rsync ensembl-otter.git
+do_rsync team_tools.git
+do_rsync zircon.git
+do_rsync github-mirror-ro/Anacode/ensembl.git github-mirror-ro/Anacode
+
+exit $?
+
