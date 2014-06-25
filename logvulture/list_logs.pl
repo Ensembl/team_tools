92d6e6c027fdc17f9aad196e2e87f6a425858a5b
Matthew Astley <mca@sanger.ac.uk>
2014-06-25 09:47:51 +0100

    logfile lister - fix over-representation of old snapshots
    The if (!defined $leaf{$log}) { was letting in
     many old copies of logfiles.
    
    Re-listed logfiles 40 hours later, halved the representation
      2370 logs.txt
      5723 logs.txt~

--- logvulture/list_logs.pl
+++ logvulture/list_logs.pl
@@ -34,18 +34,20 @@ sub main {
             my @weekly = sort { length($a) <=> length($b) or $a cmp $b }
               files_in(qr{/weekly\.}, $snapdir);
             my @more_logs = map { files_in(qr{/otterlace\..*\.log$}, $_) } @weekly;
-            my %leaf # leaf => length
-              = map {( leaf($_), -s $_ )} @recent;
+            my %leaf # leaf => [ $filename, $dev_inode ]
+              = map {( leaf($_), [ $_, dev_inode($_) ])} @recent;
             foreach my $log (@more_logs) {
                 my $leaf = leaf($log);
-                if (!defined $leaf{$log}) {
+                my $devi = dev_inode($log);
+                if (!defined $leaf{$leaf}) {
                     # new leafname
                     push @logs, $log;
-                    $leaf{$leaf} = -s $log;
-                } elsif ($leaf{$leaf} == -s $log) {
-                    # same file, no action
+                    $leaf{$leaf} = [ $log, $devi ];
+                } elsif ($leaf{$leaf}[1] eq $devi) {
+                    # same file or longer, no action
                 } else {
-                    warn "$log might be different?  Ignored it anyway";
+                    my @old = @{ $leaf{$leaf} };
+                    warn "($log $devi) might be different to (@old)?  Ignored it anyway";
                 }
             }
         }
@@ -78,6 +80,11 @@ sub leaf {
     my ($fn) = @_;
     die "leaf?! $fn" unless $fn =~ m{/([^/]+)$};
     return $1;
+}
+
+sub dev_inode {
+    my ($fn) = @_;
+    return join ':', (stat $fn)[0,1];
 }
 
 main();
