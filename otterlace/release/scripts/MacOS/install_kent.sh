6bc2423a09d79c0d9f2c0c5fc1b7a6598b4c5729
Thibaut Hourlier <thibaut@ebi.ac.uk>
2019-02-08 16:32:34 +0000

    Compile in a different directory to save space
    The kent library is taking a lot of useless disk space in our case. We
    only want jkweb.a and the header files

--- otterlace/release/scripts/MacOS/install_kent.sh
+++ otterlace/release/scripts/MacOS/install_kent.sh
@@ -4,18 +4,18 @@ set -e # bail out on error
 
 . "$( dirname "$0" )/_macos_in_app.sh" || exit 1
 lib_dir="${install_base}/lib"
-cd ${lib_dir} &&
+make_dir="${install_base}/_non_dist"
+mach_type=$(uname -m)
+cd ${make_dir} &&
 git clone --depth 1 git://genome-source.soe.ucsc.edu/kent.git &&
 cd kent/src &&
 sed -i .old "s?-lssl?${lib_dir}/libssl.a?;s?-lcrypto?${lib_dir}/libcrypto.a?" ./inc/common.mk &&
 CFLAGS=-fPIC MYSQLINC=$(${install_base}/lib/percona/bin/mysql_config --include | sed 's/-I//') MYSQLLIBS=$(${install_base}/lib/percona/bin/mysql_config --libs) SSL_DIR=${install_base} PNGLIB=${lib_dir}/libpng.a ZLIB=${lib_dir}/libz.a make userApps &&
-for D in ./*; do
-  if [ "$D" = "./inc" ] || [ "$D" = "./lib" ]; then
-    continue
-  fi
-  rm -rf "$D"
-done
-cd .. &&
-for D in .git .gitignore build confs java python; do
-  rm -rf "$D"
-done
+mkdir -p ${lib_dir}/kent/src/{lib,inc}
+mkdir ${lib_dir}/kent/src/lib/${mach_type}
+cp lib/${mach_type}/jkweb.a ${lib_dir}/kent/src/lib/${mach_type} &&
+cp inc/*.h ${lib_dir}/kent/src/inc &&
+cd ${make_dir} &&
+rm -rf kent
+
+exit $?
